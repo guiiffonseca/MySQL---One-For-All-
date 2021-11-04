70f92f4ed873872ac5f8d721c753e8bfd4406da7
Guilherme Fonseca <guilherme.fonseca@paco.local>
2021-11-04 08:44:32 -0300

    Desafio 04

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_3_artistas AS 
+SELECT 
+A.NOME AS `artista`,
+COUNT(S.ARTISTA_ID) AS `seguidores` 
+FROM SpotifyClone.Artista AS A
+JOIN SpotifyClone.Seguindo AS S ON A.ARTISTA_ID = S.ARTISTA_ID
+GROUP BY S.ARTISTA_ID
+ORDER BY `seguidores` DESC, `artista`
+LIMIT 3;
